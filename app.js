e10fed0c072aad76b194a7356886b0fc7b3f6700
Edson Vitor Vieira <edsonvitorvieira@hotmail.com>
2024-07-30 10:50:48 -0300

    mudança normal

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
+function botaoCopiar() {
+    document.querySelector('.botao__copiar').style.display = 'block';
+}
+
 // Função para criptografar o texto
 function criptografar() {
     let texto = document.getElementById('inputTexto').value;
+    if (texto.trim() !== "") {
     let textoCriptografado = texto.replace(/e/g, 'enter')
                                   .replace(/i/g, 'imes')
                                   .replace(/a/g, 'ai')
                                   .replace(/o/g, 'ober')
                                   .replace(/u/g, 'ufat');
     document.getElementById('resultadoTexto').innerText = textoCriptografado;
-}
+    mostrarResultado();
+} else {
+    esconderResultado();
+}}
+
+
 
 // Função para descriptografar o texto
 function descriptografar() {
     let texto = document.getElementById('inputTexto').value;
-    let textoDescriptografado = texto.replace(/enter/g, 'e')
+    if (texto.trim() !== "") {
+    let descriptografar = texto.replace(/enter/g, 'e')
                                      .replace(/imes/g, 'i')
                                      .replace(/ai/g, 'a')
                                      .replace(/ober/g, 'o')
                                      .replace(/ufat/g, 'u');
-    document.getElementById('resultadoTexto').innerText = textoDescriptografado;
-}
+    document.getElementById('resultadoTexto').innerText = descriptografar;
+    mostrarResultado();
+} else {
+    esconderResultado();
+}}
 
-// função para esconder a imagem e texto inicial de resultado
+// esconder o texto inicial
+function esconderResultado(img, textoInicial) {
+    let x = document.getElementById('img');
+    let y = document.getElementById('textoInicial');
+    if (x.style.display === block) {
+        x.style.display = none;
+    } else {
+        x.style.display = "block";
+      } if (y.style.display === block) {
+        y.style.display = none;
+    } else {
+        y.style.display = "block";
+      }
+    }
+
+function botaoCopiar(botaoCopiar) {
+    document.getElementById('botaoCopiar').style.display = 'none';
+}
 
 // Função para copiar o texto para a área de transferência
 function copiarTexto() {
